ab5acd5bcf3f397e0f03674e89ff0ca0cdb7f5e2
Vidar S. Ramdal <vramdal@gmail.com>
2020-07-11 06:32:53 +0200

    Skriver om tester

--- src/grid/Grid.test.ts
+++ src/grid/Grid.test.ts
@@ -1,109 +1,104 @@
 import { Grid } from "./Grid";
 
 describe("Grid", () => {
+  const onContentResizeNeededHandler = jest.fn();
+  let grid: Grid;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    grid = new Grid({ width: 5, height: 3 });
+    grid.onContentResizeNeeded(onContentResizeNeededHandler);
+  });
+
   it("should report no changes", () => {
-    const grid = new Grid({ width: 5, height: 3 });
+    grid.calculateChanges();
 
-    const result = grid.calculateChanges();
-
-    expect(result).toMatchInlineSnapshot(`
-      Object {
-        "modifiedContentPositions": Array [],
-        "newContentPositions": Array [],
-        "removedContentIds": Array [],
-      }
-    `);
+    expect(onContentResizeNeededHandler).not.toHaveBeenCalled();
   });
 
   it("should add a window", () => {
-    const grid = new Grid({ width: 5, height: 3 });
-
     grid.addContainerForContent("some window", 1);
-    const result = grid.calculateChanges();
+    grid.calculateChanges();
 
-    expect(result).toMatchInlineSnapshot(`
-      Object {
-        "modifiedContentPositions": Array [],
-        "newContentPositions": Array [
-          Object {
-            "contentId": 1,
-            "frame": Object {
-              "height": 3,
-              "width": 5,
-              "x": 0,
-              "y": 0,
+    expect(onContentResizeNeededHandler).toHaveBeenCalledWith(
+      expect.objectContaining({
+        modifiedContentPositions: [],
+        newContentPositions: [
+          {
+            contentId: 1,
+            frame: {
+              height: 3,
+              width: 5,
+              x: 0,
+              y: 0,
             },
           },
         ],
-        "removedContentIds": Array [],
-      }
-    `);
+        removedContentIds: [],
+      })
+    );
   });
 
   it("should add a second window", () => {
-    const grid = new Grid({ width: 5, height: 3 });
     grid.addContainerForContent("some window", 1);
     grid.calculateChanges();
 
     grid.addContainerForContent("some window", 2);
-    const result = grid.calculateChanges();
+    grid.calculateChanges();
 
-    expect(result).toMatchInlineSnapshot(`
-      Object {
-        "modifiedContentPositions": Array [
-          Object {
-            "contentId": 1,
-            "frame": Object {
-              "height": 1,
-              "width": 5,
-              "x": 0,
-              "y": 0,
+    expect(onContentResizeNeededHandler).toHaveBeenCalledWith(
+      expect.objectContaining({
+        modifiedContentPositions: [
+          {
+            contentId: 1,
+            frame: {
+              height: 1,
+              width: 5,
+              x: 0,
+              y: 0,
             },
           },
         ],
-        "newContentPositions": Array [
-          Object {
-            "contentId": 2,
-            "frame": Object {
-              "height": 1,
-              "width": 5,
-              "x": 0,
-              "y": 1,
+        newContentPositions: [
+          {
+            contentId: 2,
+            frame: {
+              height: 1,
+              width: 5,
+              x: 0,
+              y: 1,
             },
           },
         ],
-        "removedContentIds": Array [],
-      }
-    `);
+        removedContentIds: [],
+      })
+    );
   });
 
   it("should remove a window", () => {
-    const grid = new Grid({ width: 5, height: 3 });
     grid.addContainerForContent("some window", 1);
     grid.addContainerForContent("some window", 2);
     grid.calculateChanges();
 
     grid.removeContainerForContent(1);
-    const result = grid.calculateChanges();
+    grid.calculateChanges();
 
-    expect(result).toMatchInlineSnapshot(`
-      Object {
-        "modifiedContentPositions": Array [
-          Object {
-            "contentId": 2,
-            "frame": Object {
-              "height": 3,
-              "width": 5,
-              "x": 0,
-              "y": 0,
+    expect(onContentResizeNeededHandler).toHaveBeenCalledWith(
+      expect.objectContaining({
+        modifiedContentPositions: [
+          {
+            contentId: 2,
+            frame: {
+              height: 3,
+              width: 5,
+              x: 0,
+              y: 0,
             },
           },
         ],
-        "newContentPositions": Array [],
-        "removedContentIds": Array [
-          "1",
-        ],
-      }
-    `);
+        newContentPositions: [],
+        removedContentIds: ["1"],
+      })
+    );
   });
 });
